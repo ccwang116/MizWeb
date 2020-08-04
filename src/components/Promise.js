a8a8770125c5aee25166b4c144e112e335c46e28
Chao Chun Wang <66878095+ccwang116@users.noreply.github.com>
2020-08-04 17:59:52 +0800

    Create Promise.js

---
+++ src/components/Promise.js
@@ -0,0 +1,52 @@
+// Promise 應用
+// promise 物件建立
+// promise function 回傳 promise 物件
+// then 用法串接 (只要回傳結果為promise物件即可用)
+// Promise.all (避免使用，一次性發過多AJAX)
+// 一次送出去，需等全部回來才動作，無法控制順序
+// Promise.race 只看首個完成的 (較少用)
+
+const p1 = new Promise((resolve, reject) => {
+    // 條件: 完成 setTimeout 或 API 取得資料
+    if (true) { resolve('OK')} 
+    else { reject(new Error('ERROR')) }
+  })
+
+const promiseFunc = function (url) {
+    return new Promise((resolve, reject) => {
+        // 條件: 完成 setTimeout 或 API 取得資料
+        if (true) { resolve('OK')} 
+        else { reject(new Error('ERROR')) }
+    })
+}
+promise.then((result) => {
+    console.log(result)
+    return someFunc()
+  }).catch((err) => { console.error(err) })
+
+Promise.all([funcA(), funcB()])
+    .then((result) => {
+    console.log(result)
+}).catch((err) => { console.error(err) })
+
+// xhr + promise
+// 宣告成 promise 物件
+// 將 response 轉成物件後，resolve 傳至 result
+const test = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    const url = 'https://jsonplaceholder.typicode.com/users'
+    xhr.open('GET', url, true)
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 400) {
+        console.table(JSON.parse(xhr.response))
+        resolve(JSON.parse(xhr.response)) 
+      } else {
+        reject(new Error(`連結失敗${xhr.status}`))
+      }
+    }
+    xhr.send(null)
+  })
+
+  test.then((result) => {
+    console.table(result)
+  }).catch((err) => { console.error(err) })
