f9d507a6654c5930ba778379b1d5950546d72c7c
Chao Chun Wang <66878095+ccwang116@users.noreply.github.com>
2020-08-07 10:50:09 +0800

    Add some notes

--- src/components/Promise.js
+++ src/components/Promise.js
@@ -7,46 +7,59 @@
 // Promise.race 只看首個完成的 (較少用)
 
 const p1 = new Promise((resolve, reject) => {
-    // 條件: 完成 setTimeout 或 API 取得資料
-    if (true) { resolve('OK')} 
-    else { reject(new Error('ERROR')) }
-  })
+  // 條件: 完成 setTimeout 或 API 取得資料
+  if (true) { resolve('OK') }
+  else { reject(new Error('ERROR')) }
+})
 
 const promiseFunc = function (url) {
-    return new Promise((resolve, reject) => {
-        // 條件: 完成 setTimeout 或 API 取得資料
-        if (true) { resolve('OK')} 
-        else { reject(new Error('ERROR')) }
-    })
+  return new Promise((resolve, reject) => {
+    // 條件: 完成 setTimeout 或 API 取得資料
+    if (true) { resolve('OK') }
+    else { reject(new Error('ERROR')) }
+  })
 }
 promise.then((result) => {
-    console.log(result)
-    return someFunc()
-  }).catch((err) => { console.error(err) })
+  console.log(result)
+  return someFunc()
+}).catch((err) => { console.error(err) })
 
 Promise.all([funcA(), funcB()])
-    .then((result) => {
+  .then((result) => {
     console.log(result)
-}).catch((err) => { console.error(err) })
+  }).catch((err) => { console.error(err) })
 
 // xhr + promise
 // 宣告成 promise 物件
 // 將 response 轉成物件後，resolve 傳至 result
 const test = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
-    const url = 'https://jsonplaceholder.typicode.com/users'
-    xhr.open('GET', url, true)
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status < 400) {
-        console.table(JSON.parse(xhr.response))
-        resolve(JSON.parse(xhr.response)) 
-      } else {
-        reject(new Error(`連結失敗${xhr.status}`))
-      }
+  const xhr = new XMLHttpRequest()
+  const url = 'https://jsonplaceholder.typicode.com/users'
+  xhr.open('GET', url, true)
+  xhr.onload = () => {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      console.table(JSON.parse(xhr.response))
+      resolve(JSON.parse(xhr.response))
+    } else {
+      reject(new Error(`連結失敗${xhr.status}`))
     }
-    xhr.send(null)
-  })
+  }
+  xhr.send(null)
+})
 
-  test.then((result) => {
-    console.table(result)
-  }).catch((err) => { console.error(err) })
+test.then((result) => {
+  console.table(result)
+}).catch((err) => { console.error(err) })
+
+
+///rxjs 用在 promise
+import { from } from 'rxjs';
+
+// Create an Observable out of a promise
+const data = from(fetch('/api/endpoint'));
+// Subscribe to begin listening for async result
+data.subscribe({
+  next(response) { console.log(response); },
+  error(err) { console.error('Error: ' + err); },
+  complete() { console.log('Completed'); }
+});
