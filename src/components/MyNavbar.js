46f7f5f10b004491a4f9c3c84657c9c5cac9623c
Chao Chun Wang <66878095+ccwang116@users.noreply.github.com>
2020-08-21 16:51:06 +0800

    [navbar] button rotate img

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -3,12 +3,24 @@ import { Navbar, Nav, Button } from "react-bootstrap";
 import { Link, NavLink, withRouter } from "react-router-dom";
 import { ThemeContexts } from "../contexts/ThemeContext";
 import context from "react-bootstrap/esm/AccordionContext";
+import classes from "./styles.module.scss";
 
 function MyNavbar() {
   const contextType = ThemeContexts
   console.log(context)
   const { isLightThme, light, dark } = context
   const theme = isLightThme ? light : dark;
+  //旋轉
+  const handleCardShow = ( _type , _index , _target) => {
+    const target = _type + _index;
+
+    _target.currentTarget.classList.toggle(classes.imgRotate);
+
+    const tarDoms = document.querySelectorAll(`.${target}`);
+    for (const dom of tarDoms) {
+        dom.classList.toggle(classes.show);
+  }
+}
   return (
     <>
       <ThemeContexts.Consumer>{(context) => {
@@ -24,27 +36,30 @@ function MyNavbar() {
               <Link className="flex-fill" to="/analysis">
                 <Button className="w-100 bg-secondary" >
                   <i className="fas fa-clipboard-check mr-2"></i>單次分析
-          </Button>
+                </Button>
               </Link>
               <Link className="flex-fill" to="/chronic">
                 <Button className="w-100 topbtn-active" >
                   <i className="fas fa-chart-line mr-2"></i>Chronic
-            </Button>
+                </Button>
               </Link>
             </div>
             <Nav className="">
               <Nav.Link as={NavLink} to="/" exact>
                 Home
-          </Nav.Link>
+              </Nav.Link>
               <Nav.Link as={NavLink} to="/memberlogin">
                 Login
-          </Nav.Link>
+              </Nav.Link>
               <Nav.Link as={NavLink} to="/productlist">
                 Sign Up
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/about">
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/about">
                 About
-          </Nav.Link>
+              </Nav.Link>
+              <Button className={classes.titleInner} onClick={(e)=>{handleCardShow('stage',0,e)}}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Arrow_icon.svg/1200px-Arrow_icon.svg.png"alt=""  />
+              </Button>
             </Nav>
           </Navbar>
         )
