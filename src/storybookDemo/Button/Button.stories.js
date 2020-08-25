08fb231504667a6844bbc388f484bb22a21b7ef7
Chao Chun Wang <66878095+ccwang116@users.noreply.github.com>
2020-08-25 16:03:44 +0800

    [storybook] New addon

--- src/storybookDemo/Button/Button.stories.js
+++ src/storybookDemo/Button/Button.stories.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Button from "./Button";
 import Center from "../Center";
+import { action, actions } from "@storybook/addon-action";
+
 export default {
     title: 'Button',
     component: Button,
-    decorators: [story => <Center>{story()}</Center>]
+    // decorators: [story => <Center>{story()}</Center>]
 }
 
-export const Primary = () => <Button variant="primary" >Primary</Button>
-export const Secondary = () => <Button variant="Secondary" >Secondary</Button>
+export const Primary = () => <Button onClick={action('Click Handler')} variant="primary" >Primary</Button>
+export const Secondary = () => <Button variant="Secondary" {...actions('onClick','onMouseOver')}>Secondary</Button>
 export const Success = () => <Button variant="Success" >Success</Button>
+
+export const Log = () =>(
+    <Button onClick={()=>{console.log('clicked')}}>Log</Button>
+)
--- src/storybookDemo/start.md
+++ src/storybookDemo/start.md
@@ -10,3 +10,12 @@ create component/Button/
 please add preview.js in .storybook directory ,
 `add global decorator in it`
 so that you don't have to add  the decorator in every stories
+
+## Docs Addon
+
+`yarn add -D @storybook/addon-docs`
+
+## Others
+knobs -> change disabled and label
+viewport -> see from different device
+a11y -> see Accessiblility:Violations,Passes and Incomplete
