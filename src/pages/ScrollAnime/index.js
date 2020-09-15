1c011b556dfdc9d1fdeaeb6ffef909bdb6270f26
Ivy Wang <chaochunwang@outlook.com>
2020-09-15 14:36:26 +0800

    console window offset

--- src/pages/ScrollAnime/index.js
+++ src/pages/ScrollAnime/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React,{useRef, useEffect, useState} from 'react';
 import classes from './style.module.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(classes);
 //放大 上升 換色
 function ScrollAnime() {
+    // const contentBox = useRef();
+    // const [topnumber,setTopNumber]= useState(0)
+    function calcWH() {
+    const offset = { 
+                    top: window.scrollY, 
+                    left: window.scrollX, 
+                };
+    console.log(offset);
+    }
+    useEffect(() => {
+        window.addEventListener("scroll",()=>calcWH());
+    },);
+    
     return(
         <>
-        <div className={cx('building')}>
+        <div className={cx('building')} id="scrollCapture" data-amime="5s">
             <div className={cx('alphabet')}>
                 
             </div>
@@ -18,7 +31,7 @@ function ScrollAnime() {
                 the words are flying
             </div>
         </div>
-        <div className={cx('bus')}>
+        <div className={cx('bus')} id="bus" >
             
             </div>
         </>
