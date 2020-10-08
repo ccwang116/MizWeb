e247bd5931e55b38c961d3c7aa7ab7c2d5f95b98
Ivy Wang <chaochunwang@outlook.com>
2020-10-08 11:44:28 +0800

    Add rxjs and hook blog link

--- src/components/rxjsSubscribe.js
+++ src/components/rxjsSubscribe.js
@@ -14,3 +14,6 @@ let subscription = squaredNumbers.subscribe(result =>{
 //if keep subscribe > 9 16 25
 //if unsubscribe >  9 
 document.getElementById("app").innerHTML = `<h1>hello</h1>`
+
+//更多hook + rxjs的寫法
+//https://jasonwatmore.com/post/2020/04/21/react-hooks-rxjs-communicating-between-components-with-observable-subject
