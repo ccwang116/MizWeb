fe3c75cd9f9753723ada07a5a9f704f5c7eddebc
Ivy Wang <chaochunwang@outlook.com>
2021-04-06 15:07:28 +0800

    [Mention] use draft mention

---
+++ src/components/MentionTag/index.js
@@ -0,0 +1,234 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { EditorState, getDefaultKeyBinding, RichUtils } from "draft-js";
+import Editor from "draft-js-plugins-editor";
+import Draft from "draft-js";
+import createMentionPlugin, {
+  defaultSuggestionsFilter,
+} from "draft-js-mention-plugin";
+import { stateToHTML } from "draft-js-export-html";
+import "draft-js-mention-plugin/lib/plugin.css";
+import "draft-js/dist/Draft.css";
+import _ from "lodash";
+
+const cmdState = {
+  handled: "handled",
+  notHandled: "not-handled",
+};
+const emptyContentState = Draft.convertFromRaw({
+  entityMap: {},
+  blocks: [
+    {
+      text: "",
+      key: "wholeInputBlock",
+      type: "unstyled",
+      entityRanges: [],
+    },
+  ],
+});
+//000
+function convertInitialContent(text) {
+  let i = 0;
+  let resultAll = {
+    entityMap: {},
+    blocks: [
+      {
+        text: text,
+        key: "wholeInputBlock",
+        type: "unstyled",
+        entityRanges: [],
+      },
+    ],
+  };
+  text.replace(/\@(\w+)/g, (match, name, offset) => {
+    resultAll.entityMap[i] = {
+      data: {
+        mention: {
+          display: name,
+          id: i,
+          name,
+        },
+      },
+      mutability: "IMMUTABLE",
+      type: "mention",
+    };
+    resultAll.blocks[0].entityRanges.push({
+      key: i,
+      offset: offset,
+      length: match.length,
+    });
+    i++;
+  });
+  console.log(resultAll);
+  return resultAll;
+}
+const initialContentState = Draft.convertFromRaw(
+  convertInitialContent("這編輯的字 @Foo 測試 @Bar 測試 @Hey ")
+);
+//編輯文字方法一
+// const html = `這裡的function會自己轉換 <i>這是斜體字</i>`;
+// const blocksFromHTML = Draft.convertFromHTML(html);
+// const initialContentState = Draft.ContentState.createFromBlockArray(
+//     blocksFromHTML.contentBlocks,
+//     blocksFromHTML.entityMap
+// );
+//編輯文字方法二
+const initialContentState3 = Draft.convertFromRaw({
+  entityMap: {
+    0: {
+      data: {
+        mention: {
+          display: "Foo",
+          id: "1",
+          name: "Foo",
+        },
+      },
+      mutability: "IMMUTABLE",
+      type: "mention",
+    },
+    1: {
+      data: {
+        mention: {
+          display: "Bar",
+          id: "2",
+          name: "Bar",
+        },
+      },
+      mutability: "IMMUTABLE",
+      type: "mention",
+    },
+  },
+  blocks: [
+    {
+      text: "這編輯的字 Foo  Bar 自己key位置",
+      key: "wholeInputBlock",
+      type: "unstyled",
+      entityRanges: [
+        { key: 0, length: 3, offset: 6 },
+        { key: 1, length: 3, offset: 11 },
+      ],
+    },
+  ],
+});
+class PostEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.mentionPlugin = createMentionPlugin({
+      entityMutability: "IMMUTABLE",
+    });
+    this.state = {
+      editorState: EditorState.createWithContent(initialContentState),
+      suggestions: this.props.mentions,
+    };
+  }
+  reset = () => {
+    this.setState({
+      editorState: EditorState.createWithContent(emptyContentState),
+    });
+  };
+
+  onSearchChange = ({ value }) => {
+    this.setState({
+      suggestions: defaultSuggestionsFilter(value, this.props.mentions),
+    });
+  };
+
+  keyBindingFn = (e) => {
+    // retrun custom commands on keyPress if required
+    return getDefaultKeyBinding(e);
+  };
+
+  toHtml = () => {
+    const contentState = this.state.editorState.getCurrentContent();
+    const raw = Draft.convertToRaw(contentState);
+    const mentionArr = Object.values(raw.entityMap).map(
+      (val) => val.data.mention
+    );
+    console.log(raw);
+    const options = {
+      entityStyleFn: (entity) => {
+        const entityType = entity.get("type").toLowerCase();
+        if (entityType === "mention") {
+          const data = entity.getData();
+          return {
+            element: "M",
+          };
+        }
+      },
+    };
+    const strarr = stateToHTML(contentState, options).split(" ");
+    let i = -1;
+    const result = strarr
+      .map((sm) => {
+        if (sm.match(/<M(([\s\S])*?)<\/M>/g)) {
+          i++;
+          return sm.replace(
+            /<M(([\s\S])*?)<\/M>/g,
+            `<!M[{"n":${mentionArr[i].name},"i":${mentionArr[i].id}}]>`
+          );
+        } else {
+          return sm;
+        }
+      })
+      .join(" ")
+      .replace(/\<p\>|\<\/p\>/g, "");
+    return result;
+  };
+  onChange = (editorState) => {
+    const { setValue } = this.props;
+    this.setState({
+      editorState,
+    });
+    setValue(this.toHtml());
+  };
+  handleKeyCommand = (command) => {
+    // handle custom command here;
+    const newState = RichUtils.handleKeyCommand(
+      this.state.editorState,
+      command
+    );
+    if (newState) {
+      this.onChange(newState);
+      return cmdState.handled;
+    }
+    return cmdState.notHandled;
+  };
+  render() {
+    const { MentionSuggestions } = this.mentionPlugin;
+    const plugins = [this.mentionPlugin];
+    const { className, style, placeholder } = this.props;
+    return (
+      <div className={`editor ${className}`} style={style}>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          plugins={plugins}
+          keyBindingFn={this.keyBindingFn}
+          handleKeyCommand={this.handleKeyCommand}
+          placeholder={placeholder}
+          ref={(element) => {
+            this.editor = element;
+          }}
+        />
+
+        <MentionSuggestions
+          onSearchChange={this.onSearchChange}
+          suggestions={this.state.suggestions}
+        />
+      </div>
+    );
+  }
+}
+
+PostEditor.propTypes = {
+  mentions: PropTypes.arrayOf(PropTypes.any),
+  className: PropTypes.string,
+  style: PropTypes.object,
+  placeholder: PropTypes.string,
+};
+
+PostEditor.defaultProps = {
+  mentions: [],
+};
+
+export default PostEditor;
--- src/pages/DemoMention.js
+++ src/pages/DemoMention.js
@@ -1,25 +1,53 @@
-import React from "react";
-import Taggable from "./Taggable";
-
+import React, { useState, useRef } from "react";
+import MentionTag from "../components/MentionTag";
 function About(props) {
+  const [value, setValue] = useState("");
+  const mentionsRef = useRef();
+  const style = {
+    PostEditor__container: {
+      width: "600px",
+      backgroundColor: "#fff",
+      boxShadow: "0 3px 6px 0 rgba(0, 0, 0, 0.4)",
+      transition: "all 0.1s ease-out",
+    },
+    PostEditor: {
+      border: "0",
+      boxSizing: "border-box",
+      fontFamily: "ripplingFontBook",
+      fontSize: "16px",
+      height: "auto",
+      minHeight: "140px",
+      outline: "none",
+      padding: "20px",
+      resize: "none",
+      width: "100%",
+    },
+  };
   return (
     <>
-      <div
-        style={{
-          width: "80%",
-          height: "768px",
-          background: "#eee",
-          margin: "50px auto",
-        }}
-      >
-        <div
-          style={{
-            width: "250px",
-            border: "1px solid black",
-            margin: "50px auto",
-          }}
-        >
-          <Taggable />
+      <div style={style.PostEditor__container}>
+        <div style={style.PostEditor}>
+          <MentionTag
+            className="PostEditor__input"
+            placeholder={"What are you thinking"}
+            value={value}
+            setValue={setValue}
+            mentions={[
+              { name: "Foo", id: "1", display: "Foo", profile_picture: null },
+              { name: "Bar", id: "2", display: "Bar", profile_picture: null },
+            ]}
+            ref={mentionsRef}
+          />
+          <div key="footerMain" className="PostEditor__footer">
+            <button
+              title="Show"
+              onClick={() => {
+                alert(value);
+              }}
+            >
+              Save
+            </button>
+          </div>
         </div>
       </div>
     </>
