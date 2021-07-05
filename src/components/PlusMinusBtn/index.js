162f0c9928807b62e99a05c233c80e1a069be03b
Ivy Wang <chaochunwang@outlook.com>
2021-07-05 17:08:40 +0800

    [PlusButton] width bigger

--- src/components/PlusMinusBtn/index.js
+++ src/components/PlusMinusBtn/index.js
@@ -1,32 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { InputGroup, Button, FormControl } from "react-bootstrap";
-function PlusMinusBtn({ prodLabel, price, onChange }) {
+function PlusMinusBtn({ data, onChange }) {
   const [value, setValue] = useState(0);
   const handlePlus = (label, e) => {
     setValue(value + 1);
   };
   const handleMinus = (label, e) => {
-    setValue(value - 1);
+    if (value > 0) {
+      setValue(value - 1);
+    }
   };
   const handleChangeAmount = (label, e) => {
-    console.log(+e);
     setValue(e);
   };
   useEffect(() => {
-    onChange(value, prodLabel, price);
+    onChange(value, data);
   }, [value]);
   return (
-    <InputGroup className="mb-3" size="sm" style={{ width: "90px" }}>
-      <Button variant="light" size="sm" onClick={() => handleMinus(prodLabel)}>
+    <InputGroup className="mb-3" size="lg" style={{ width: "180px" }}>
+      <Button variant="light" size="lg" onClick={() => handleMinus(data.label)}>
         <i class="fas fa-minus"></i>
       </Button>
       <FormControl
         type="text"
         value={value}
         className="text-center"
-        onChange={(evt) => handleChangeAmount(prodLabel, evt.target.value)}
+        onChange={(evt) => handleChangeAmount(data.label, evt.target.value)}
       />
-      <Button variant="light" size="sm" onClick={() => handlePlus(prodLabel)}>
+      <Button variant="light" size="lg" onClick={() => handlePlus(data.label)}>
         <i class="fas fa-plus"></i>
       </Button>
     </InputGroup>
